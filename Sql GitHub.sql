e2b4e76a5bffd5332f177c11c1dee9194cded8fb
Fabian Sanchez Aguilar <fabiansa@oficinas.mx.cij>
2024-05-24 14:30:45 -0600

    uno

---
+++ Sql GitHub.sql
@@ -0,0 +1,7 @@
+
+-- Online SQL Editor to Run SQL Online.
+-- Use the editor to create new tables, insert data and all other SQL operations.
+  
+SELECT first_name, age
+FROM Customers
+Where customer_id < 3
